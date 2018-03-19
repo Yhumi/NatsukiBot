bbcbf210b2573670c281747074f32ed7a639221d
CynicalPopcorn <cynicalpopcorn@gmail.com>
2018-03-19 00:37:49 +0000

    Change list layout

--- MonikAIBot/Modules/Administration.cs
+++ MonikAIBot/Modules/Administration.cs
@@ -646,7 +646,7 @@ namespace MonikAIBot.Modules
 
             foreach (Waifus w in Ws)
             {
-                EmbedFieldBuilder efb = new EmbedFieldBuilder().WithName(w.ID.ToString()).WithValue(w.Waifu);
+                EmbedFieldBuilder efb = new EmbedFieldBuilder().WithValue(w.ID.ToString() + ". " + w.Waifu);
 
                 embed.AddField(efb);
             }
